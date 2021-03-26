4a87fd59e92a5d736e4151f84dde4055a0129afc
Themis Theodoratos <themistheodoratos@gmail.com>
2021-03-26 17:38:53 +0000

    Multilingual setup

---
+++ accounts/messages.py
@@ -0,0 +1,32 @@
+signUp = {
+    'tabTitle'          : 'Genesis | Sign Up',
+    'pageTitle'         : 'Create an account',
+    'loginUrl1'         : 'Already have an account?',
+    'loginUrl2'         : 'Login',
+    'signUpButton'      : 'Create an account',
+    
+}   
+
+ 
+updateUser = {
+    'tabTitle'          : 'Genesis | Update details',
+    'pageTitle'         : 'Account details',
+    'updateButton'      : 'Update Details',
+    'changePassword1'   : 'Do you want to change your password?',
+    'changePassword2'   : 'Change password',
+}
+
+settings = {
+    'tabTitle'          : 'Genesis | Settings',
+    'pageTitle'         : 'SETTINGS',
+    'pageSubTitle'      : 'General Settings',
+    'accountDetails'    : 'Account details',
+    'accountDetailsText': 'Update your account information such as username, email, first and last name etc.',
+    'Go'                : 'Go',
+    'changePassword'    : 'Change password',
+    'changePasswordText': 'Click below to change your current password.',
+
+}
+
+
+
--- accounts/views.py
+++ accounts/views.py
@@ -2,16 +2,23 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.defaulttags import register
+from .messages import signUp, updateUser, settings
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
-
+        
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['msg'] = signUp
+        return context
     
 class UpdateUserView(LoginRequiredMixin, generic.UpdateView):
     
@@ -19,9 +26,18 @@ class UpdateUserView(LoginRequiredMixin, generic.UpdateView):
     success_url = reverse_lazy('landing-page')
     template_name = 'registration/updateUser.html'
     
-    # get current user object
     def get_object(self, queryset=None): 
         return self.request.user
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['msg'] = updateUser
+        return context
 
 class SettingsView(LoginRequiredMixin, generic.TemplateView):
     template_name = "settings.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['msg'] = settings
+        return context
