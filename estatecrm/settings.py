2d506731e65aca08b8b5f07a7b1545a4d1762fd4
Themis Theodoratos <themistheodoratos@gmail.com>
2021-10-03 10:17:53 +0100

    PWA config_3

--- estatecrm/settings.py
+++ estatecrm/settings.py
@@ -185,10 +185,10 @@ DATETIME_INPUT_FORMATS = ['%d-%m-%Y %H:%M:%S']
 
 # PWA
 PWA_APP_NAME = 'Yeneses'
-PWA_APP_DESCRIPTION = "Property search engine"
+PWA_APP_DESCRIPTION = "Yeneses - Property search engine"
 PWA_APP_THEME_COLOR = '#3b82f6'
 PWA_APP_BACKGROUND_COLOR = '#ffffff'
-PWA_APP_DISPLAY = 'fullscreen'
+PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/'
 PWA_APP_ORIENTATION = 'any'
 PWA_APP_START_URL = '/'
@@ -199,12 +199,12 @@ PWA_APP_ICONS = [
         'src': '/static/images/yeneses_fav_bg160.png',
         'sizes': '160x160',
         "purpose": "any maskable"
+    },
+    {
+        'src': '/static/images/yeneses_fav_bg.png',
+        'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
+        'sizes': '512x512'
     }
-    # {
-    #     'src': '/static/images/yeneses_fav_bg160.png',
-    #     'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
-    #     'sizes': '512x512'
-    # }
 ]
 PWA_APP_ICONS_APPLE = [
     {
