f514cb32e8caff6bdc42f5d5bd83493d8ef6a9a1
Themis Theodoratos <themistheodoratos@gmail.com>
2021-11-10 17:01:18 +0000

    Verbose names for profile picture, is_realtor fields

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from properties.validators import validate_file_size
+from django.utils.translation import ugettext_lazy as _
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    is_realtor = models.BooleanField(default=False)
-    is_agent = models.BooleanField(default=False)
-    profile_picture = models.ImageField(upload_to='profile_picture/%Y/%m/%d/', blank=True, validators=[validate_file_size])
+    is_realtor = models.BooleanField(default=False, verbose_name=_('Signed up as realtor?'))
+    is_agent = models.BooleanField(default=False, verbose_name=_('Signed up as agent?'))
+    profile_picture = models.ImageField(upload_to='profile_picture/%Y/%m/%d/', blank=True, validators=[validate_file_size], verbose_name=_('Profile picture'))
     
     
     def __str__(self):
